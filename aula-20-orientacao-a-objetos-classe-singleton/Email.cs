b4c2a1062ea47eb04655ec7a43ee9220d2fc143d
nicollas <nicollaslopesjf@gmail.com>
2018-08-30 21:08:21 -0300

    file

---
+++ aula-20-orientacao-a-objetos-classe-singleton/Email.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_20_orientacao_a_objetos_classe_singleton
+{
+    public class Email
+    {
+        private Email() { }
+
+        private static Email instancia;
+        public string Origem; 
+        public string Destino; 
+        public string Titulo; 
+        public string Corpo;
+
+        public void EnviarEmail()
+        {
+            Console.WriteLine("Enviando e-mail para " + Destino + "\nCom o titulo: " + Titulo + "\nCorpo: " + Corpo);
+        }
+
+        public static Email Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Email();
+                }
+
+                return instancia;
+            }
+
+        }
+    }
+}
---
+++ aula-20-orientacao-a-objetos-classe-singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_20_orientacao_a_objetos_classe_singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Email.Instancia.Corpo = "bla bla";
+            Email.Instancia.Titulo = "Titulo do email";
+            Email.Instancia.Origem = "nicollas";
+            Email.Instancia.Destino = "lopes";
+            Email.Instancia.EnviarEmail();
+
+            Console.Read();
+        }
+    }
+}
